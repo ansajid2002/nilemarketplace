9327e9ae74fe9e7adc6e6de9840e8872d6957773
sajid <ansajid2002@gmail.com>
2024-03-12 12:13:28 +0530

    changed add money to wallet screen

--- screens/wallet/AddMoney.js
+++ screens/wallet/AddMoney.js
@@ -3,7 +3,7 @@ import { AntDesign, Ionicons } from '@expo/vector-icons';
 import React, { useEffect, useState } from 'react';
 import { formatCurrency } from './Wallet';
 import { useSelector } from 'react-redux';
-import { AdminUrl } from '../../constant';
+import { AdminUrl, HeaderBar } from '../../constant';
 import { TextInput } from 'react-native';
 import { ActivityIndicator } from 'react-native';
 import { t } from 'i18next';
@@ -158,172 +158,11 @@ const AddMoney = ({ navigation }) => {
     >
       <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
         <StatusBar barStyle="dark-content" backgroundColor="white" />
-        <View style={{ flex: 1, position: 'relative' }}>
-          <View style={{ height: '25%', backgroundColor: '#02BAEE', alignItems: 'center', justifyContent: 'center' }}>
-          </View>
-          <TouchableOpacity
-            style={{
-              position: 'absolute',
-              top: 20,
-              left: 20,
-              zIndex: 1,
-            }}
-            onPress={() => navigation.goBack()}
-          >
-            <AntDesign name="arrowleft" size={24} color="white" />
-          </TouchableOpacity>
-          <View style={{
-            position: 'absolute',
-            top: 100,
-            left: 0,
-            right: 0,
-            backgroundColor: 'white',
-            padding: 20,
-            margin: 10,
-            borderRadius: 10,
-            shadowColor: 'gray',
-            elevation: 5
-          }}
+        <HeaderBar navigation={navigation} title={""} goback={true} searchEnable={false} cartEnable={false} />
+        <View className="px-4">
 
-
-          >
-            {/* Content for the absolute positioned View */}
-            <View className="flex-row justify-between">
-              <Text className="text-xl font-bold">{t("NGMP Token")}</Text>
-              <Ionicons name="wallet-outline" size={24} color={'blue'} />
-            </View>
-            <Text className="mt-2 text-xl tracking-wide font-semibold">{formatCurrency(parseFloat(walletTotal))}</Text>
-            <View className="flex-row space-x-5 py-2 justify-center mt-5 ">
-              {
-                navigationOptions.map((item, index) => (
-                  <TouchableOpacity key={index} onPress={() => navigation.navigate(item.screen)}>
-                    <View key={index} style={{ alignItems: 'center', marginBottom: 10, width: 100 }}>
-                      <View className="bg-blue-100 p-2 rounded-full">
-                        <AntDesign name={item.icon} size={24} color="#496197" />
-                      </View>
-                      <Text style={{ textAlign: 'center', flexWrap: 'wrap', fontSize: 12 }} className="text-gray-600 mt-2 font-semibold ">{t(`${item.label}`)}</Text>
-                    </View>
-                  </TouchableOpacity>
-                ))
-              }
-            </View>
-          </View>
-
-          <View style={{
-            backgroundColor: 'white',
-            padding: 20,
-            margin: 10,
-            borderRadius: 10,
-            shadowColor: 'gray',
-            elevation: 7,
-            position: 'relative',
-            top: 150
-          }}>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{t("Add Money to Wallet")}</Text>
-            </View>
-
-            <View>
-              {/* Options for selecting EVC or eDahab */}
-              <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-
-                <TouchableOpacity
-                  style={{
-                    borderWidth: 1,
-                    borderColor: selectedOption === 'eDahab' ? 'blue' : '#ccc',
-                    borderRadius: 5,
-                    padding: 10,
-                    width: '48%',
-                    alignItems: 'center',
-                  }}
-                  onPress={() => handleOptionSelect('eDahab')}
-                >
-                  <Text style={{ color: selectedOption === 'eDahab' ? 'blue' : 'black' }}>eDahab</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                  style={{
-                    borderWidth: 1,
-                    borderColor: selectedOption === 'EVC' ? 'blue' : '#ccc',
-                    borderRadius: 5,
-                    padding: 10,
-                    width: '48%',
-                    alignItems: 'center',
-                  }}
-                  onPress={() => handleOptionSelect('EVC')}
-                >
-                  <Text style={{ color: selectedOption === 'EVC' ? 'blue' : 'black' }}>EVC</Text>
-                </TouchableOpacity>
-
-              </View>
-            </View>
-
-            {/* Input for entering custom amount */}
-            <TextInput
-              style={{
-                borderWidth: 1,
-                borderColor: '#ccc',
-                borderRadius: 5,
-                padding: 10,
-                marginTop: 10,
-              }}
-              placeholder={selectedOption === 'EVC' ? "Enter 9 digit EVC Number" : "Enter 9 digit eDahab Number"}
-              keyboardType="numeric"
-              value={edahabNumber}
-              onChangeText={handleedahabNumberChange}
-            />
-
-            <TextInput
-              style={{
-                borderWidth: 1,
-                borderColor: '#ccc',
-                borderRadius: 5,
-                padding: 10,
-                marginTop: 10,
-              }}
-              placeholder="Enter amount"
-              keyboardType="numeric"
-              value={amount}
-              onChangeText={handleAmountChange}
-            />
-
-            {/* Options for predefined amounts */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-              {[100, 500, 1000, 5000].map((option, index) => (
-                <TouchableOpacity
-                  key={index}
-                  style={{
-                    borderWidth: 1,
-                    borderColor: '#ccc',
-                    borderRadius: 5,
-                    padding: 10,
-                    width: '24%',
-                    alignItems: 'center',
-                  }}
-                  onPress={() => handleAmountSelect(option)}
-                >
-                  <Text>{`${formatCurrency(option).endsWith('.00') ? formatCurrency(option).slice(0, -3) : formatCurrency(option)}`}</Text>
-                </TouchableOpacity>
-              ))}
-            </View>
-
-            {/* Button to add money */}
-            <TouchableOpacity
-              style={{
-                backgroundColor: '#013766',
-                borderRadius: 5,
-                padding: 15,
-                marginTop: 20,
-                alignItems: 'center',
-              }}
-              onPress={!loadingButton && handleAddMoney}
-            >
-              {
-                loadingButton ? <ActivityIndicator color={'white'} /> : <Text style={{ color: 'white', fontSize: 16 }}>{t("Add Money")}</Text>
-
-              }
-            </TouchableOpacity>
-          </View>
+        <Text className="text-center text-2xl font-medium mt-6 mb-4">Welcome back!</Text>
+        <Text className="text-lg text-gray-700 text-center">To recharge your wallet, please visit <Text className="font-bold text-[#00008b]">Nilegmp.com</Text>, Once your wallet is recharged, you can use the balance to purchase products, send Nile Tokens to your friends, and enjoy various services offered by the app.  </Text>
         </View>
 
       </SafeAreaView>
@@ -332,3 +171,184 @@ const AddMoney = ({ navigation }) => {
 };
 
 export default AddMoney;
+
+
+// return (
+//   <KeyboardAvoidingView
+//     behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+//     style={{ flex: 1, backgroundColor: 'white' }}
+//     className="transition-all duration-75 ease-in-out"
+//   >
+//     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+//       <StatusBar barStyle="dark-content" backgroundColor="white" />
+//       <View style={{ flex: 1, position: 'relative' }}>
+//         <View style={{ height: '25%', backgroundColor: '#02BAEE', alignItems: 'center', justifyContent: 'center' }}>
+//         </View>
+//         <TouchableOpacity
+//           style={{
+//             position: 'absolute',
+//             top: 20,
+//             left: 20,
+//             zIndex: 1,
+//           }}
+//           onPress={() => navigation.goBack()}
+//         >
+//           <AntDesign name="arrowleft" size={24} color="white" />
+//         </TouchableOpacity>
+//         <View style={{
+//           position: 'absolute',
+//           top: 100,
+//           left: 0,
+//           right: 0,
+//           backgroundColor: 'white',
+//           padding: 20,
+//           margin: 10,
+//           borderRadius: 10,
+//           shadowColor: 'gray',
+//           elevation: 5
+//         }}
+
+
+//         >
+//           {/* Content for the absolute positioned View */}
+//           <View className="flex-row justify-between">
+//             <Text className="text-xl font-bold">{t("NGMP Token")}</Text>
+//             <Ionicons name="wallet-outline" size={24} color={'blue'} />
+//           </View>
+//           <Text className="mt-2 text-xl tracking-wide font-semibold">{formatCurrency(parseFloat(walletTotal))}</Text>
+//           <View className="flex-row space-x-5 py-2 justify-center mt-5 ">
+//             {
+//               navigationOptions.map((item, index) => (
+//                 <TouchableOpacity key={index} onPress={() => navigation.navigate(item.screen)}>
+//                   <View key={index} style={{ alignItems: 'center', marginBottom: 10, width: 100 }}>
+//                     <View className="bg-blue-100 p-2 rounded-full">
+//                       <AntDesign name={item.icon} size={24} color="#496197" />
+//                     </View>
+//                     <Text style={{ textAlign: 'center', flexWrap: 'wrap', fontSize: 12 }} className="text-gray-600 mt-2 font-semibold ">{t(`${item.label}`)}</Text>
+//                   </View>
+//                 </TouchableOpacity>
+//               ))
+//             }
+//           </View>
+//         </View>
+
+//         <View style={{
+//           backgroundColor: 'white',
+//           padding: 20,
+//           margin: 10,
+//           borderRadius: 10,
+//           shadowColor: 'gray',
+//           elevation: 7,
+//           position: 'relative',
+//           top: 150
+//         }}>
+//           <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+//             <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{t("Add Money to Wallet")}</Text>
+//           </View>
+
+//           <View>
+//             {/* Options for selecting EVC or eDahab */}
+//             <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+
+//               <TouchableOpacity
+//                 style={{
+//                   borderWidth: 1,
+//                   borderColor: selectedOption === 'eDahab' ? 'blue' : '#ccc',
+//                   borderRadius: 5,
+//                   padding: 10,
+//                   width: '48%',
+//                   alignItems: 'center',
+//                 }}
+//                 onPress={() => handleOptionSelect('eDahab')}
+//               >
+//                 <Text style={{ color: selectedOption === 'eDahab' ? 'blue' : 'black' }}>eDahab</Text>
+//               </TouchableOpacity>
+
+//               <TouchableOpacity
+//                 style={{
+//                   borderWidth: 1,
+//                   borderColor: selectedOption === 'EVC' ? 'blue' : '#ccc',
+//                   borderRadius: 5,
+//                   padding: 10,
+//                   width: '48%',
+//                   alignItems: 'center',
+//                 }}
+//                 onPress={() => handleOptionSelect('EVC')}
+//               >
+//                 <Text style={{ color: selectedOption === 'EVC' ? 'blue' : 'black' }}>EVC</Text>
+//               </TouchableOpacity>
+
+//             </View>
+//           </View>
+
+//           {/* Input for entering custom amount */}
+//           <TextInput
+//             style={{
+//               borderWidth: 1,
+//               borderColor: '#ccc',
+//               borderRadius: 5,
+//               padding: 10,
+//               marginTop: 10,
+//             }}
+//             placeholder={selectedOption === 'EVC' ? "Enter 9 digit EVC Number" : "Enter 9 digit eDahab Number"}
+//             keyboardType="numeric"
+//             value={edahabNumber}
+//             onChangeText={handleedahabNumberChange}
+//           />
+
+//           <TextInput
+//             style={{
+//               borderWidth: 1,
+//               borderColor: '#ccc',
+//               borderRadius: 5,
+//               padding: 10,
+//               marginTop: 10,
+//             }}
+//             placeholder="Enter amount"
+//             keyboardType="numeric"
+//             value={amount}
+//             onChangeText={handleAmountChange}
+//           />
+
+//           {/* Options for predefined amounts */}
+//           <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+//             {[100, 500, 1000, 5000].map((option, index) => (
+//               <TouchableOpacity
+//                 key={index}
+//                 style={{
+//                   borderWidth: 1,
+//                   borderColor: '#ccc',
+//                   borderRadius: 5,
+//                   padding: 10,
+//                   width: '24%',
+//                   alignItems: 'center',
+//                 }}
+//                 onPress={() => handleAmountSelect(option)}
+//               >
+//                 <Text>{`${formatCurrency(option).endsWith('.00') ? formatCurrency(option).slice(0, -3) : formatCurrency(option)}`}</Text>
+//               </TouchableOpacity>
+//             ))}
+//           </View>
+
+//           {/* Button to add money */}
+//           <TouchableOpacity
+//             style={{
+//               backgroundColor: '#013766',
+//               borderRadius: 5,
+//               padding: 15,
+//               marginTop: 20,
+//               alignItems: 'center',
+//             }}
+//             onPress={!loadingButton && handleAddMoney}
+//           >
+//             {
+//               loadingButton ? <ActivityIndicator color={'white'} /> : <Text style={{ color: 'white', fontSize: 16 }}>{t("Add Money")}</Text>
+
+//             }
+//           </TouchableOpacity>
+//         </View>
+//       </View>
+
+//     </SafeAreaView>
+//   </KeyboardAvoidingView>
+// );
